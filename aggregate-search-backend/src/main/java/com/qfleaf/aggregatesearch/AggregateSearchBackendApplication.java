bcbe581c597ff2fb1a88c33725a88a702d0a81e5
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-18 10:40:36 +0800

    初始化后端

---
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/AggregateSearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qfleaf.aggregatesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregateSearchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregateSearchBackendApplication.class, args);
+    }
+
+}
