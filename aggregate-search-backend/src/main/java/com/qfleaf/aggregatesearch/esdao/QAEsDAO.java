b9a563094f304fdae1bbc4daeb422c460ee046c5
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-25 18:37:55 +0800

    初步实现es全文搜索功能

--- aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/esdao/QAEsDAO.java
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/esdao/QAEsDAO.java
@@ -1,7 +1,11 @@
 package com.qfleaf.aggregatesearch.esdao;
 
 import com.qfleaf.aggregatesearch.model.entity.QA;
+import com.qfleaf.aggregatesearch.model.vo.QAListVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface QAEsDAO extends ElasticsearchRepository<QA, Long> {
+    Page<QAListVO> searchByQuestionOrAnswerOrAuthorOrderByCreatedTime(String question, String answer, String author, Pageable pageable);
 }
