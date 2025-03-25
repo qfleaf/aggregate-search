e0f91b57568adb88fde542482fe5454dd1ddd0aa
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-25 11:07:39 +0800

    优化返回类型

--- aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/adapter/SearchService.java
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/adapter/SearchService.java
@@ -1,14 +1,14 @@
 package com.qfleaf.aggregatesearch.adapter;
 
+import com.qfleaf.aggregatesearch.common.PageData;
 import com.qfleaf.aggregatesearch.model.SearchRequest;
-import org.springframework.data.domain.Page;
 
-public interface SearchService<T> {
+public interface SearchService {
     /**
      * 执行搜索操作
      *
      * @param searchRequest 搜索请求参数
      * @return 结果分页
      */
-    Page<T> search(SearchRequest searchRequest);
+    PageData search(SearchRequest searchRequest);
 }
