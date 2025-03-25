4f2e93c7e36c57e0791b72327c7b0180ba31b15f
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-25 15:55:57 +0800

    优化pagedata的构造

--- aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/common/PageData.java
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/common/PageData.java
@@ -1,12 +1,21 @@
 package com.qfleaf.aggregatesearch.common;
 
 import lombok.Data;
+import org.springframework.data.domain.Page;
 
 @Data
 public class PageData {
     private Object records;
     private Long total;
-    private Long pages;
-    private Long current;
-    private Integer size;
+    private int pages;
+    private int current;
+    private int size;
+
+    public PageData(Page<?> page) {
+        this.records = page.getContent();
+        this.total = page.getTotalElements();
+        this.pages = page.getTotalPages();
+        this.current = page.getNumber();
+        this.size = page.getSize();
+    }
 }
