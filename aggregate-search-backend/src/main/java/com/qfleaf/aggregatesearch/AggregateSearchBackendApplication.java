cd09afeff8a46559805b2d0f67daf8c7eea72558
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-25 11:02:11 +0800

    添加实体与持久层相关的扫描包注解

--- aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/AggregateSearchBackendApplication.java
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/AggregateSearchBackendApplication.java
@@ -2,8 +2,14 @@ package com.qfleaf.aggregatesearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.qfleaf.aggregatesearch.repository")
+@EnableElasticsearchRepositories(basePackages = "com.qfleaf.aggregatesearch.esdao")
+@EntityScan(basePackages = "com.qfleaf.aggregatesearch.model.entity")
 public class AggregateSearchBackendApplication {
 
     public static void main(String[] args) {
