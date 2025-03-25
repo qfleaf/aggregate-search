8c103f6702af31ef3fa3f6f2a3782f03c1184246
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-25 18:28:18 +0800

    启用定时任务配置

--- aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/AggregateSearchBackendApplication.java
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/AggregateSearchBackendApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.qfleaf.aggregatesearch.repository")
 @EnableElasticsearchRepositories(basePackages = "com.qfleaf.aggregatesearch.esdao")
 @EntityScan(basePackages = "com.qfleaf.aggregatesearch.model.entity")
+@EnableScheduling
 public class AggregateSearchBackendApplication {
 
     public static void main(String[] args) {
