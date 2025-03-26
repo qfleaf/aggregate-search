090cc5931aeab0513872505cb4cfbb717affc978
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-26 17:30:09 +0800

    挂载router

--- aggregate-search-web/src/main.ts
+++ aggregate-search-web/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import router from './router'
 import './style.css'
 import App from './AggregateSearch.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
