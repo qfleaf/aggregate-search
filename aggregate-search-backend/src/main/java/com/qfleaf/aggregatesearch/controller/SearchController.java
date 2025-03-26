12f3ffa663d86291a973d24b2b0f52aef7f0e64b
qfleaf <160916892+qfleaf@users.noreply.github.com>
2025-03-26 17:37:58 +0800

    优化统一响应结果

--- aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/controller/SearchController.java
+++ aggregate-search-backend/src/main/java/com/qfleaf/aggregatesearch/controller/SearchController.java
@@ -1,8 +1,11 @@
 package com.qfleaf.aggregatesearch.controller;
 
 import com.qfleaf.aggregatesearch.adapter.SearchServiceAdapter;
+import com.qfleaf.aggregatesearch.common.CommonResponse;
 import com.qfleaf.aggregatesearch.common.PageData;
+import com.qfleaf.aggregatesearch.common.ResponseCode;
 import com.qfleaf.aggregatesearch.model.SearchRequest;
+import com.qfleaf.aggregatesearch.utils.ResultUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +25,8 @@ public class SearchController {
     }
 
     @GetMapping
-    public PageData search(SearchRequest searchRequest) {
+    public CommonResponse<PageData> search(SearchRequest searchRequest) {
         log.debug("params: {}", searchRequest);
-        return searchServiceAdapter.search(searchRequest);
+        return ResultUtil.success(ResponseCode.SUCCESS, searchServiceAdapter.search(searchRequest));
     }
 }
